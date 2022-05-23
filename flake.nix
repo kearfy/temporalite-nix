89435e7568c3d3a730c66a43d57f0d178e605580
Micha de Vries <contact@michadevries.nl>
2022-05-23 11:26:16 +0200

    Temporalite repository now as input

--- flake.nix
+++ flake.nix
@@ -4,23 +4,17 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs";
         flake-utils.url = "github:numtide/flake-utils";
+        temporalite.url = "github:DataDog/temporalite?rev=0797d11e124e64b068be86566ef6577d80563c9c";
+        temporalite.flake = false;
     };
 
-    outputs = { self, nixpkgs, flake-utils }:
+    outputs = { self, nixpkgs, flake-utils, temporalite }:
         flake-utils.lib.eachDefaultSystem (system:
             with nixpkgs.legacyPackages.${system}; rec {
                 defaultPackage = buildGoModule rec {
                     name = "temporalite-${version}";
-                    commit = "0797d11e124e64b068be86566ef6577d80563c9c";
                     version = "1.15.1";
-
-                    src = fetchFromGitHub {
-                        owner = "DataDog";
-                        repo = "temporalite";
-                        rev = "${commit}";
-                        sha256 = "sha256-a0MzHLuNw/+VlPG+QO1QxEmalYk1qMVjjvBtL2pYqmQ=";
-                    };
-
+                    src = temporalite;
                     subPackages = [ "cmd/temporalite" ];
                     vendorSha256 = "sha256-1UQ3qparXahO7ox/H4Begt9h/sGI3sN5R7NssxshQo8=";
                     doCheck = false;
