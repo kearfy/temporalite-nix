c7a612760131b62f550a09c2696ac12fc14b17ed
Micha de Vries <contact@michadevries.nl>
2022-05-23 19:58:20 +0200

    Now using latest version with buildGo118Module.

--- flake.nix
+++ flake.nix
@@ -4,19 +4,18 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs";
         flake-utils.url = "github:numtide/flake-utils";
-        temporalite.url = "github:DataDog/temporalite?rev=0797d11e124e64b068be86566ef6577d80563c9c";
+        temporalite.url = "github:DataDog/temporalite";
         temporalite.flake = false;
     };
 
     outputs = { self, nixpkgs, flake-utils, temporalite }:
         flake-utils.lib.eachDefaultSystem (system:
             with nixpkgs.legacyPackages.${system}; rec {
-                defaultPackage = buildGoModule rec {
-                    name = "temporalite-${version}";
-                    version = "1.15.1";
+                defaultPackage = buildGo118Module rec {
+                    name = "temporalite";
                     src = temporalite;
                     subPackages = [ "cmd/temporalite" ];
-                    vendorSha256 = "sha256-1UQ3qparXahO7ox/H4Begt9h/sGI3sN5R7NssxshQo8=";
+                    vendorSha256 = "sha256-z8p9uyxxAiSV/zG1DqpQ01dALhVxjeGWQxOl4BP/BRU=";
                     doCheck = false;
                 };
 
